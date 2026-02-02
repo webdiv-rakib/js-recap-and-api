fa4d974d71ac0889e7756e3705b7fb4e8196a235
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-02-02 14:13:22 +0600

    var,let,const

---
+++ let-const.js
@@ -0,0 +1,14 @@
+// const name = "Bangladesh"
+// name = 'BD';
+// console.log(name);
+let price = 120;
+price = 100;
+console.log(price);
+
+const dress = ['shirt', 'pant'];
+dress.push('maflar');
+console.log(dress);
+
+const student = { name: 'rakib', marks: 50 };
+student.marks = 40;
+console.log(student);
