d00f6467379571c4c28fdf1ecd92fac73972c534
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-02-02 22:21:24 +0600

    More Arrow functions and arrow functions =>

---
+++ more-arrow.js
@@ -0,0 +1,11 @@
+const getTax = (amount, taxRate) => amount * taxRate / 100;
+const add = (p, q) => p + q;
+const taxDetails = getTax(10000, 50);
+const result = add(2, 2);
+console.log(result);
+console.log(taxDetails);
+
+// anonymous function
+document.getElementById("").addEventListener('click', () => {
+
+})
