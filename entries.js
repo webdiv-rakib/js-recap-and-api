b3547d0eb2391e81ab215c57579eddffa6b0a379
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-02-02 23:50:43 +0600

    keys, values, entries.

---
+++ entries.js
@@ -0,0 +1,12 @@
+const employee = {
+    name: 'rakib',
+    designation: 'HR',
+    salary: '20000',
+    experience: 2
+}
+const keys = Object.keys(employee);
+console.log(keys);
+const values = Object.values(employee);
+console.log(values);
+const entries = Object.entries(employee);
+console.log(entries);
