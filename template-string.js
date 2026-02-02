26289a7e0b7f0289d5c316d8a3d7a4282c77360d
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-02-02 21:51:57 +0600

    1-3 Template string, multiple line string, dynamic string (` `)

---
+++ template-string.js
@@ -0,0 +1,10 @@
+const poem = `am pata jora jora
+marbo chabuk chorbo gora
+ore babu ghura dara
+ashche amar pagla ghora`
+console.log(poem);
+
+function sum(num1,num2){
+    const result = num1+num2;
+    const outPut = `sum of${num1}`
+}
